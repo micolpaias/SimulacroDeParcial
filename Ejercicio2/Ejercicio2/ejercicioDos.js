e4df449221dd135263ab3df5283990aca929db4a
Micol Paias <micol.paias.barboza@accenture.com>
2021-03-02 22:14:25 -0300

    Ejercicio2

--- Ejercicio2/Ejercicio2/ejercicioDos.js
+++ Ejercicio2/Ejercicio2/ejercicioDos.js
@@ -11,79 +11,84 @@ function probarEjercicio(){
 	var cantidadTemperatura;
 	var cantidadPaises;
 	var menorCantidadHabitates;
+	var suma;
 	
 	cantidadTemperatura = 0; 
 	cantidadPaises = 0; 
-
+	suma = 0; 
 	for (i= 0; i < 2; i++) {
 		do 
 		{
 			continenteIngresado = prompt("Ingrese el continente");
-		} while (continenteIngresado != "America" && continenteIngresado != "Asia" && continenteIngresado != "Europa" && continenteIngresado != "Africa" && continenteIngresado != "Oceania")
-		{
-			continenteIngresado = prompt("Intente nuevamente. Ingrese el continente");
-		}
+		} while (continenteIngresado != "America" && continenteIngresado != "Asia" && continenteIngresado != "Europa" && continenteIngresado != "Africa" && continenteIngresado != "Oceania");
 		do 
 		{
 			nombrePais = prompt("Ingrese el nombre del pais");
-		} while (isNaN(nombrePais) == false)
-		{
-			continenteIngresado = prompt("Intente nuevamente. Ingrese el nombre del pais");
-		}
+		} while (isNaN(nombrePais) == false);
 		do 
 		{
 			numerodeHabitantes = prompt("Ingrese la cantidad de habitantes por pais");
-		} while (isNaN(numerodeHabitantes) == true && numerodeHabitantes < 0 || numerodeHabitantes > 7001 )
-		{
-			continenteIngresado = prompt("Intente nuevamente. Ingrese la cantidad de habitantes por pais");
-		}
+		} while (isNaN(numerodeHabitantes) == true && numerodeHabitantes < 0 || numerodeHabitantes > 7001 );
 		do 
 		{
 			estadoEconomico = prompt("Ingrese el nivel de probreza");
-		} while (estadoEconomico != "Pobre" && estadoEconomico != "Rico" && estadoEconomico != "Muy Rico" || (estadoEconomico == "Pobre" && continenteIngresado == "Europa"))
-		{
-			continenteIngresado = prompt("Intente nuevamente. Ingrese el nivel de probreza");
-		}
+			while (estadoEconomico == "Pobre" && continenteIngresado == "Europa")
+			{
+				estadoEconomico = prompt("No hay paises pobres en Europa, por favor ingrese nuevamente el nivel de pobreza");
+			}
+		} while (estadoEconomico != "Pobre" && estadoEconomico != "Rico" && estadoEconomico != "Muy Rico" );
 		do 
 		{
 			temperaturaMinima = prompt("Ingrese la temperatura minima ");
-		} while (isNaN(temperaturaMinima) == true && temperaturaMinima < -51 || temperaturaMinima > 51)
-		{
-			continenteIngresado = prompt("Intente nuevamente. Ingrese la temperatura minima");
-		};
+		} while (isNaN(temperaturaMinima) == true && temperaturaMinima < -51 || temperaturaMinima > 51);
 	}
 
 			//a - cantidad temperaturas pares
-			if (temperaturaMinima % 2 == 0) {
-				cantidadTemperatura++;
-				alert(cantidadTemperatura);
-			}
-			//b - cantidad temperaturas impares en europa
-			if (temperaturaMinima % 2 != 0) 
+			if (temperaturaMinima % 2 == 0) 
 			{
+				cantidadTemperatura++;
+				alert("Hay " + cantidadTemperatura + " temperaturas pares");
+			}
+			else{
+			//b - cantidad temperaturas impares en europa
 				if (continenteIngresado == "Europa")
 				{
 					cantidadTemperatura++;
-					alert(cantidadTemperatura);
+					alert("Hay " + cantidadTemperatura + " temperaturas impares en Europa");
 				}
 			}
 			//c - El nombre del pais con menos habitantes
-			menorCantidadHabitates = Math.min(temperaturaMinima);
+			menorCantidadHabitates = new Array;
+			menorCantidadHabitates = menorCantidadHabitates.push(numerodeHabitantes);
+			menorCantidadHabitates = Math.min(menorCantidadHabitates);
+		
+		
 
 			//d - cantidad de paises que superen los 40 g
 			if (temperaturaMinima < 39) 
 			{
-				alert(cantidadTemperatura);
+				cantidadPaises++;
+				alert("Hay " + cantidadPaises + " paises que superan los 40 grados");
 			}
-
 			//e - cantidad de paises de america menos de 0 grados
-			if (temperaturaMinima > 1) 
+			else 
 			{
 				if (continenteIngresado == "America")
 				{
-					cantidadTemperatura++;
-					alert(cantidadTemperatura);
+					cantidadPaises++;
+					alert("Hay " + cantidadPaises + " paises que tienen menos de 0 grados grados");
 				}
 			}
-			
+
+			//f - promedio de hab entre paises ingresados
+
+			for ( i = 0; i < cantidadPaises; i++ ) 
+			{
+				if ( cantidadPaises[i] > 0 )
+				{
+					suma = numerodeHabitantes++;
+				}
+			operacionPromedio = suma / cantidadPaises
+			alert("El promedio de habitantes es: " +operacionPromedio )
+			} 
 }
